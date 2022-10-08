9fa111866fc199991ed5911b27823391bfc0ad6b
DefinitelyNotAngel <86986999+DefinitelyNotAngel@users.noreply.github.com>
2022-10-08 10:47:36 +0300

    commitment

---
+++ bot.py
@@ -0,0 +1,54 @@
+#import pynacl
+import random
+import asyncio
+from discord.ext.commands import Bot
+import discord
+from discord.ext import commands,tasks 
+import time
+
+BOT_PREFIX = "`"
+TOKEN = "MTAyNjQ5NzMyMjAwMTEwOTAyMg.GOiYn2.JQend8HmDRgnUAGzA6rYGPSMjYVfKbEtmeEVJk"
+
+intents = discord.Intents().all()
+
+client = Bot(command_prefix=BOT_PREFIX, intents = intents)
+
+@client.command(name='ping',  # ping
+                description="kinda shows what latency the bot has",
+                brief="base ping command",
+                aliases=['pp'])
+async def ping(ctx):
+    await ctx.channel.send(f'Pong! Responded in {round(client.latency * 1000)} ms')
+
+
+@client.command(name='test',  # test
+                description="just a test",
+                brief="test",
+                aliases=['t'],
+                pass_context=True)
+async def test(ctx):
+#   possible_responses = [
+#        'this worked!'
+#        'this worked 2!'
+#    ]
+    await ctx.channel.send('this finally works!!!!') # has no attribute say???? #fixedish #! need to fix the command not picking a random choice the command: random.choice(possible.responses)
+
+@client.command(name='ding',  # ding lmao
+                description="another form for the ping command",
+                brief="other form of ping",
+                aliases=['dd'])
+async def ding(ctx):
+    await ctx.channel.send(f'dong! Responded in {round(client.latency * 1000)} ms')
+
+@client.command(name='pinger', # pinger 
+                description="pings you lmao",
+                brief="pings you",
+                aliases=['pmp'])
+async def pinger(ctx):
+    await ctx.channel.send(ctx.message.author.mention + ' get pinged fucker')
+
+@client.event
+async def on_ready():
+    print('bot is up and running')
+
+client.run(TOKEN)
