dd4142abf664927f7b5bb438b4fbef3c3acce359
DefinitelyNotAngel <86986999+DefinitelyNotAngel@users.noreply.github.com>
2022-10-08 11:28:05 +0300

    commitment7

--- bot.py
+++ bot.py
@@ -13,23 +13,23 @@ client = commands.Bot(command_prefix=BOT_PREFIX, intents = intents)
 
 @client.command()
 async def load(ctx: Context, extension):
-    client.load_extension(f'cogs.{extension}')
+    client.load_extension(f'commands.{extension}')
     await ctx.send("Loaded Cog")
 
 @client.command()
 async def unload(ctx: Context, extension):
-    client.unload_extension(f'cogs.{extension}')
+    client.unload_extension(f'commands.{extension}')
     await ctx.send("Unloaded Cog")
 
 @client.command()
 async def reload(ctx: Context, extension):
-    client.unload_extension(f'cogs.{extension}')
-    client.load_extension(f'cogs.{extension}')
+    client.unload_extension(f'commands.{extension}')
+    client.load_extension(f'commands.{extension}')
     await ctx.send("Reloaded Cog")
 
 for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
-        client.load_extension(f'cogs.{filename[0:-3]}')
+        client.load_extension(f'commands.{filename[0:-3]}')
 
 @client.event
 async def on_ready():
