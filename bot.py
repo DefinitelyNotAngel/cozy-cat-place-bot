b303d09c06ed9316af3928940499fc9f2f26801a
DefinitelyNotAngel <86986999+DefinitelyNotAngel@users.noreply.github.com>
2022-10-25 21:15:36 +0300

    memes?

--- bot.py
+++ bot.py
@@ -111,7 +111,7 @@ async def meme(ctx):
     content = get("https://meme-api.herokuapp.com/gimme").text
     data = json.loads(content,)
     meme = discord.Embed(title=f"{data['title']}", Color = discord.Color.random()).set_image(url=f"{data['url']}")
-    await ctx.reply(embed=meme)
+    await ctx.send(embed=meme)
 
 @client.event
 async def on_ready():
